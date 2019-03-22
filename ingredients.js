e016c448d96b6ccf218053f92aaeb9d4868ba484
Nelson Mendez <nelmen2509@gmail.com>
2019-03-22 03:39:04 +0000

    ingredients.js three loops that list every item in the array, both front to back and back to front

---
+++ ingredients.js
@@ -0,0 +1,24 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+var i = 0
+
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (j = 0; j < ingredients.length; j++) {
+  console.log(ingredients[j]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+var k = (ingredients.length - 1)
+
+ while (k >= 0) {
+  console.log(ingredients[k]);
+  k--;
+}
